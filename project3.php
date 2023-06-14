e3b2f7fdf4429ab5a04f6eb3bf9c1a24c9e4dfbb
maplestory <136591728+mapelstory@users.noreply.github.com>
2023-06-14 21:26:19 +0900

    Add files via upload

--- project3.php
+++ project3.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>finish</title>
     <style>
         .logo{
             text-align: center;
@@ -36,6 +36,7 @@
         }
     </style>
 </head>
+
 <body>
     <div class="container">
         <div class="logo">
@@ -47,4 +48,5 @@
         <p>Thank you for using our site!</p>
     </div>
 </body>
+
 </html>
